bb66c355a742d3c27e5a28dd161e674f113bbbdc
Lee Mighdoll <lee@digiting.com>
2009-03-31 13:00:24 -0700

    added another comment.

--- src/example/ObserverableAnnotation.java
+++ src/example/ObserverableAnnotation.java
@@ -1,3 +1,6 @@
 package example;
 
+/* This is currently unused.  matching the annotation with aspectJ 
+ * didn't work in my first attempt and I haven't explored it further.  */ 
+
 public @interface observable {}
